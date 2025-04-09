d350926ae131a1614d6e8c48a187f2f735b5867e
Gouri Biswas <gouribiswas011@gmail.com>
2025-04-09 23:02:48 +0530

    Created the templates and implemented api key

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,37 @@
 import React from 'react'
 import ImageUpload from './ImageUpload'
 import ImagePreview from './ImagePreview'
+import { useState } from 'react'
+import { enhancedImageAPI } from '../utils/enhancedImageAPI'
 
 const Home = () => {
+  const [uploadImage, setUploadImage] = useState(null);
+  const[enhancedImage, setEnhancedImage] = useState(null);
+  const[loading, setloading] = useState(false);
+
+  const UploadImageHandler =async (file) => {
+    setUploadImage(URL.createObjectURL(file));
+    setloading(true);
+    //call the API to enhnace the image
+    try {
+      const enhancedURL = await enhancedImageAPI(file);
+      setEnhancedImage(enhancedURL);
+      setloading(false);
+  } catch (error) {
+      console.log(error);
+      alert("Error while enhancing the image. Please try again later.");
+  }
+  };
+
   return (
-    <div>
-     <ImageUpload/>
-     <ImagePreview/>
-    </div>
+    <>
+     <ImageUpload UploadImageHandler ={UploadImageHandler } />
+     <ImagePreview 
+     loading={loading}
+    uploaded = {uploadImage}
+    enhanced = {enhancedImage}
+    />
+    </>
   )
 }
 

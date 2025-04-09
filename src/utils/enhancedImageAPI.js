b814a7f5d19ec5e502382e009acd4447cd35ee35
Gouri Biswas <gouribiswas011@gmail.com>
2025-04-09 23:18:26 +0530

    updates

--- src/utils/enhancedImageAPI.js
+++ src/utils/enhancedImageAPI.js
@@ -1,3 +1,8 @@
 export const enhancedImageAPI = async(file) => {
-  
-};
+  try {
+    //code to upload image
+    // fetch enhanced image
+  } catch (error) {
+    console.log("Error enhancing image:", error.message);
+  }
+};           

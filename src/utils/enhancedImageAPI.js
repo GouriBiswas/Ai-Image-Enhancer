19408f6a1e014bd88ec6ef2e91e903fae8258256
Gouri Biswas <gouribiswas011@gmail.com>
2025-04-11 22:58:21 +0530

    api key

--- src/utils/enhancedImageAPI.js
+++ src/utils/enhancedImageAPI.js
@@ -1,3 +1,6 @@
+const API_KEY = wxkh2eaz207dlwjnt
+const BASE_URL = "https://techhk.aoscdn.com/api/tasks/visual/scale/";         
+
 export const enhancedImageAPI = async(file) => {
   try {
     //code to upload image
